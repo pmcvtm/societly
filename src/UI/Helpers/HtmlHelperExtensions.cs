17ea4f0a1e769c4e16d41e5accdcf9001a290e5c
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2015-06-29 01:32:24 -0500

    spruce up the look a little bit

---
+++ src/UI/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace UI.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString FormGroupFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, MvcHtmlString editor)
+        {
+            var formGroup = new TagBuilder("div");
+            formGroup.AddCssClass("pure-control-group");
+
+            var label = htmlHelper.LabelFor(expression);
+
+            var validationMessage = new TagBuilder("div");
+            validationMessage.AddCssClass("validation-error");
+            validationMessage.InnerHtml = htmlHelper.ValidationMessageFor(expression).ToString();
+
+            formGroup.InnerHtml = label + "\n" + editor + "\n" + validationMessage;
+            return new MvcHtmlString(formGroup.ToString());
+        }
+    }
+}
